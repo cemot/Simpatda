4c2edffe1346b277b43408d1ebd913e33cbb0109
sanayaCorp <revisoft783@gmail.com>
2015-05-01 14:22:54 +0700

    Sistem informasi pendapatan asset daerah build versi 1.0.

---
+++ dashboard.php
@@ -0,0 +1,11 @@
+<?php
+    /**
+     * @author Afes Oktavianus
+     * @copyright 2015 Afes Oktavianus
+     * @version 1.0
+     * Import template utama 
+     */
+    include_once('template/header.php');
+    include_once('template/content.php');
+    include_once('template/footer.php');
+?>
