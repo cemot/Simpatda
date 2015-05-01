839095184d3e702907a7a9fb1a877cb1171afbd4
sanayaCorp <revisoft783@gmail.com>
2015-05-01 13:52:28 +0700

    Sistem informasi pendapatan asset daerah build versi 1.0

---
+++ config/dbstuff.inc
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * @author: afes oktavianus
+     * @abstract: file for connection application with db 
+     * @version: 1.0
+     * @copyright Copyright (c) 2015, Afes Oktavianus
+     */
+    define("DBHOST", "localhost");
+    define("DBUSER", "root");
+    define("DBPASS", "revisoft012686");
+    define("DB", "sny_simpatda");
+    
+?>
---
+++ master/jenis_usaha/list.php
@@ -0,0 +1,19 @@
+<?php require_once('../../config/functions.inc'); ?>
+<!--
+  @author: afes oktavianus
+  @abstract: file list for get table sny_m_jusaha
+  @version: 1.0
+  @copyright Copyright (c) 2015, Afes Oktavianus  
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
